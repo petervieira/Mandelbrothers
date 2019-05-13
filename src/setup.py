ed9ed4a41937ee49d9fefdb82bd5da75a3784e4d
Peter Vieira <39200345+petervieira@users.noreply.github.com>
2019-05-12 23:41:48 -0400

    Merge branch 'master' of https://github.com/petervieira/Mandelbrothers

--- src/setup.py
+++ src/setup.py
@@ -2,7 +2,6 @@ import cx_Freeze
 
 executables = [cx_Freeze.Executable('mandelbrothers.py')]
 options = {'build_exe': {'packages': ['pygame'],
-                         'include_files': ['images', 'maps', 'music', 'sounds', 'mapp.py', 'minimap.py', 'settings.py', 'sprites.py', 'text.py'],
-                         'build_exe': '../build'}}
+                         'include_files': ['images', 'maps', 'music', 'sounds', 'mapp.py', 'minimap.py', 'settings.py', 'sprites.py', 'text.py']}}
 
 cx_Freeze.setup(name = 'Mandelbrothers', options = options, executables = executables)
